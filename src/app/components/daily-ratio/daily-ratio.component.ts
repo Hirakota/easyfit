b9563713781ff3830954566060a7ff5e029660f0
Hirakota <askodmail@gmail.com>
2021-04-29 16:48:56 +0300

    angular theme

---
+++ src/app/components/daily-ratio/daily-ratio.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-ratio',
+  templateUrl: './daily-ratio.component.html',
+  styleUrls: ['./daily-ratio.component.scss'],
+})
+export class DailyRatioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
